5a8faf67632b982812295b7b6754dcf785a463ad
nachovidondo <nachovidondo@gmail.com>
2021-05-03 11:22:42 -0300

    New templates

--- AdoptionResponsable/Index/urls.py
+++ AdoptionResponsable/Index/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
-from .views import ListIndex, AdminIndex, IndexUpdate, AdminIndexImages, IndexImagesUpdate
+from . import views
+
 
 
 urlpatterns = [
-    path('',login_required(ListIndex.as_view()), name="index"),
-    path('admin_index',AdminIndex.as_view(), name="admin_index"),
-    path('edit_index/<int:pk>/',IndexUpdate.as_view(), name="edit_index"),
-    path('admin_index_images',AdminIndexImages.as_view(), name="admin_index_images"),
-    path('edit_index_images/<int:pk>/',IndexImagesUpdate.as_view(), name="edit_index_images"),
+    path('home/',views.home_view, name='home')
 ]
--- AdoptionResponsable/Index/views.py
+++ AdoptionResponsable/Index/views.py
@@ -1,33 +1,12 @@
 from django.shortcuts import render
-from .models import Index, IndexImages
-from django.views.generic import ListView, UpdateView
-from django.urls import reverse_lazy
-from . forms import FormEditIndex, FormEditIndexImages
 
 
 
-class ListIndex(ListView):
-    model = Index
-    template_name = 'index.html'
-    context_object_name = 'index'
-    
-#Administration
-class AdminIndex(ListView):
-    model = Index
-    template_name = 'admin_index.html'
-
-class IndexUpdate(UpdateView):
-    model = Index
-    template_name = 'edit_index.html'
-    form_class = FormEditIndex
-    success_url = reverse_lazy('admin_index')
-
-class AdminIndexImages(ListView):
-    model = IndexImages
-    template_name = 'admin_index_images.html'
-    
-class IndexImagesUpdate(UpdateView):
-    model = IndexImages
-    template_name = 'edit_index_images.html'
-    form_class = FormEditIndexImages
-    success_url = reverse_lazy('admin_index')
+def home_view(request):
+    user = request.user
+    hello = 'Hello World'
+    context ={
+        'user' : user, 
+        'hello' : hello,
+    }
+    return render(request,'home.html',{'user':user,'hello':hello})
