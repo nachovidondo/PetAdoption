ef26cd4751fa99e681ee372b1502c5741ed14e0d
nachovidondo <nachovidondo@gmail.com>
2021-05-05 09:30:28 -0300

    Post and comments views -template

---
+++ AdoptionResponsable/Post/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Post, Comment
+
+
+
+class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows':2}))
+    
+    class Meta:
+        model = Post
+        fields = ('content', 'image')
+        
+
+class  CommentModelForm(forms.ModelForm):
+    body = forms.CharField(label='', widget=forms.TextInput(
+        attrs={'placeholder':'Añade un comentario'}))
+    
+    class Meta:
+        model = Comment
+        fields = ('body',)
